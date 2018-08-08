13aed93dc32ad481dc0581b6afbc770b8548b4aa
Juan Carlos Quirino <juanquirinoc@gmail.com>
2018-08-08 11:00:19 -0500

    -

--- connect_db.py
+++ connect_db.py
@@ -4,11 +4,13 @@ import os
 is_prod = os.environ.get('IS_HEROKU', None)
 
 if is_prod:
-        uri = f'mongodb://{user}:{password}@ds153851.mlab.com:53851/gorditos'
+        uri = f'mongodb://admin:dO5p3rr1@ds153851.mlab.com:53851/gorditos'
+        
 else:
         user       = os.environ['DB_USER']
         password   = os.environ['DB_PASS']
-        uri = f'mongodb://admin:dO5p3rr1@ds153851.mlab.com:53851/gorditos'
+        uri = f'mongodb://{user}:{password}@ds153851.mlab.com:53851/gorditos'
+        
 
 #Database connection
 try:
