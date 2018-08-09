8440c1d9cec029975d4eb3df63aa9b3c5c1e8c83
Juan Carlos Quirino <juanquirinoc@gmail.com>
2018-08-08 19:03:48 -0500

    Removed a line because i can

--- connect_db.py
+++ connect_db.py
@@ -5,7 +5,6 @@ user       = os.environ['DB_USER']
 password   = os.environ['DB_PASS']
 uri        = f'mongodb://{user}:{password}@ds153851.mlab.com:53851/gorditos'
         
-
 #Database connection
 try:
         connect(db='gorditos',
